15895e5a479e8926a3c88a48b5f15fce6e689464
160108Sasin <sasinmaharjan96@gmail.com>
2026-04-20 12:18:21 +0545

    report page created

---
+++ src/pages/Reports.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Reports = () => {
+  return (
+    <div className="p-8 text-center text-gray-500 italic">
+      Reports UI
+    </div>
+  );
+};
+
+export default Reports;
