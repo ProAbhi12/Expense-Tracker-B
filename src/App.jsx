132eefc3db82d1d535693131a1d889adff2a17fa
Abhisekh Shakya <130886435+ProAbhi12@users.noreply.github.com>
2026-04-20 13:55:08 +0545

    Revert "Sasin added report page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,8 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import MainLayout from "./components/layout/MainLayout";
-import Dashboard from "./pages/Dashboard";
-import Transactions from "./pages/Transactions";
-import Reports from "./pages/Reports";
-
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="transactions" element={<Transactions />} />
-        <Route
-          path="categories"
-          element={
-            <div className="p-8 text-center text-gray-500 italic">
-              Categories UI{" "}
-            </div>
-          }
-        />
-        <Route
-          path="reports"
-          element={<Reports />
-          }
-        />
-        <Route
-          path="settings"
-          element={
-            <div className="p-8 text-center text-gray-500 italic">
-              Settings UI{" "}
-            </div>
-          }
-        />
-      </Route>
-    </Routes>
+    <>
+      <p className='text-red-500'>Expense tracker</p>
+    </>
   );
 }
 
