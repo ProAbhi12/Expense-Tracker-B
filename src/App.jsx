9ce78f2117e9e87ac3ac70598b3d7df68a43fadc
Aarohan-004 <022bim004@sxc.edu.np>
2026-04-20 12:54:08 +0545

    Updated Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
-          <Route path="transactions" element={<Transactions />} />
+          <Route path="/transactions" element={<Transactions />} />
           <Route
             path="categories"
             element={
