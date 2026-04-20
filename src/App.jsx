e31697cc63673459e404be6cf17d3a5139f8c116
sachin015 <malisachin015@gmail.com>
2026-04-20 11:30:57 +0545

    pulled merged with teamlead and sachin

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import React from "react";
-import { Routes ,Router , Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
 
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="transactions" element={<Transactions />} />
-          <Route
-            path="categories"
-            element={
-              <div className="p-8 text-center text-gray-500 italic">
-                Categories UI{" "}
-              </div>
-            }
-          />
-          <Route
-            path="reports"
-            element={
-              <div className="p-8 text-center text-gray-500 italic">
-                Reports UI{" "}
-              </div>
-            }
-          />
-          <Route
-            path="settings"
-            element={
-              <div className="p-8 text-center text-gray-500 italic">
-                Settings UI{" "}
-              </div>
-            }
-          />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="transactions" element={<Transactions />} />
+        <Route
+          path="categories"
+          element={
+            <div className="p-8 text-center text-gray-500 italic">
+              Categories UI{" "}
+            </div>
+          }
+        />
+        <Route
+          path="reports"
+          element={
+            <div className="p-8 text-center text-gray-500 italic">
+              Reports UI{" "}
+            </div>
+          }
+        />
+        <Route
+          path="settings"
+          element={
+            <div className="p-8 text-center text-gray-500 italic">
+              Settings UI{" "}
+            </div>
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
 
