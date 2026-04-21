b6c4e442f6280e2463bffcc2a63732ec6f24bebd
ProAbhi12 <shakyaabhisekh466@gmail.com>
2026-04-21 23:16:51 +0545

    Fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Budgets from "./pages/Category";
+import Reports from "./pages/Reports";
+import { ThemeProvider } from "./context/ThemeContext";
+import { AppProvider } from "./context/AppContext";
+
+const SettingsPlaceholder = () => (
+  <div className="p-8 text-center text-gray-500 italic">
+    Settings UI delegated to Team Lead
+  </div>
+);
+
 function App() {
   return (
-    <>
-      <p className='text-red-500'>Expense tracker</p>
-    </>
+    <AppProvider>
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="transactions" element={<Transactions />} />
+              <Route path="categories" element={<Budgets />} />
+              <Route path="reports" element={<Reports />} />
+              <Route path="settings" element={<SettingsPlaceholder />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
 
